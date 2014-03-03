7ee2ccf803c7698c75a23b242f934a21f2b04ae7
Bryce Kerley <bkerley@brycekerley.net>
2014-03-02 20:54:42 -0500

    change event names

--- monome/examples/canvas.rb
+++ monome/examples/canvas.rb
@@ -1,11 +1,11 @@
 require File.join(File.dirname(__FILE__), "..", "lib", "monome")
 
 class Canvas < Monome::Application
-  on :press do |column, row, state|
+  on :key do |column, row, state|
     grid.invert and stop if row == 7 && column == 7
   end
   
-  on :press do |column, row, state|
+  on :key do |column, row, state|
     toggle(row, column) if state == 1
   end
 end
